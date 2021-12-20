420152c8d83742ad9207588461ed2d73d5c47b48
jcarteaga1 <arteagajr07@gmail.com>
2021-12-19 23:09:13 -0500

    feat: test template pr

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Test environment staging',
+        message: 'Change to test template pr',
         input: event,
       },
       null,
